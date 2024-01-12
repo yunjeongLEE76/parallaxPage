f5e908f960b4e4a55ef8382dd57ea0ff142edf7b
yunjeongLEE76 <98930874+yunjeongLEE76@users.noreply.github.com>
2024-01-13 00:26:29 +0900

    parallaxPage ing

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@ let scrollNum = 0;
 
 const imageAll = document.querySelectorAll(".imageWrap .parallaxImage");
 const totalNum = imageAll.length;
+const subPageImage = document.querySelector(".subPage .parallaxImage");
+let x = 0;
+let targetX = 0;
+const speed = 0.1;
 
 window.addEventListener("scroll", () => {
   scrollNum = window.scrollY;
@@ -41,3 +45,19 @@ window.addEventListener("scroll", () => {
   //     scrollNum / (2 * (totalNum - 5))
   //   }px)`;
 });
+
+window.addEventListener("mousemove", (e) => {
+  x = e.pageX - innerWidth / 2;
+});
+
+const loop = () => {
+  targetX += (x - targetX) * speed;
+
+  imageAll[4].style.transform = `scale(1.1) translateX(${-targetX / 150}px)`;
+  imageAll[5].style.transform = `scale(1.1) translateX(${-targetX / 70}px)`;
+  subPageImage.style.transform = `scale(1.1) translateX(${-targetX / 20}px)`;
+  // scale은 그림의 크기를 1.1배키운다
+
+  window.requestAnimationFrame(loop);
+};
+loop();
